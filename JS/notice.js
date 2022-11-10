228527493143a8ad4c9452a0984ef862545c0483
LESEHY <tmdgus2496@gmail.com>
2022-11-10 17:19:03 +0900

    ss

--- JS/notice.js
+++ JS/notice.js
@@ -1,6 +1,19 @@
 // 
-$(()=>{
-    const notice = $(".notice li");
+$(() => {
+    const notice = $(".notice");
+    const nota = $(".notice li");
 
-    
+    nota.each((idx,ele)) => {
+        
+    }
+    // console.log(x);
+    notice.append(nota.eq(0)).fadeOut(600).delay(500).fadeIn(600)
+
+
+
+
+
+
+
+
 }); // JQB
