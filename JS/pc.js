4ad8077634f8bb36f4ab6c22a1112d3c55d4ee79
LESEHY <tmdgus2496@gmail.com>
2022-12-06 16:33:20 +0900

    ss

--- JS/pc.js
+++ JS/pc.js
@@ -33,6 +33,22 @@ $(() => {
         }
     }); // scroll
 
+
+    // 공지사항 
+    
+    const notice = $("#notice");
+
+    function noticeAuto() {
+        
+            $("#notice li").eq(0).fadeOut(500,()=>{
+                notice.append($("#notice li").eq(0));
+                $("#notice li").eq(0).fadeIn(500);
+            });
+    } // noticeAuto 함수
+
+
+    setInterval(noticeAuto,3000);
+
     
     
 
