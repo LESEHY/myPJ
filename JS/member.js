a06f53f4884dd9329eb0c9a8c4cee13eb65943e3
LESEHY <tmdgus2496@gmail.com>
2022-12-07 15:33:12 +0900

    ss

--- JS/member.js
+++ JS/member.js
@@ -1,6 +1,27 @@
 // member.js
 
 $(() => {
+    // 남 | 여 클릭
+    const male = $(".male");
+    const female = $(".female");
+
+    male.click(()=>{
+        female.removeClass("on");
+        male.addClass("on");
+    }); // click
+
+    female.click(()=>{
+        male.removeClass("on");
+        female.addClass("on");
+    }); // click
+
+
+
+
+
+
+
+    // 안내창 
     const icwar = $(".icwar");
     const ictxt = $(".ictxt");
     const none = $(".none");
